89c2fb85690d4a30cbf328637963f1e7819931ae
matth <matthieu.pons@epita.fr>
2022-05-16 10:18:16 +0200

    add loop before treatment

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
-
 import flagpy as fp
 def main():
-    fp.display('Ivory Coast')
-    print(2)
+    listCountries = fp.get_country_list()
+    print(listCountries)
+    df = fp.get_flag_df()
+    
+    for i in range(len(df)): 
+        print(df["flag"][i])
+        print(fp.flag_dist('Luxembourg', 'The Netherlands'))
 
 
 if __name__ == '__main__':
